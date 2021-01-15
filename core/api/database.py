a15806b91d51f322d0ddcfbe9eb5857744d8d559
lorty1 <orsi_wilfried@hotmail.fr>
2021-01-15 10:30:39 +0100

    lint correction for api folder

--- core/api/database.py
+++ core/api/database.py
@@ -3,14 +3,16 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_api.db"
-#SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://will:Djibouti1788!@db:3306/products_db"
-#SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://will:Djibouti1788!@localhost:3306/products_db"
+# SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://will:Djibouti1788! \
+# @db:3306/products_db"
+# SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://will:Djibouti1788! \
+# @localhost:3306/products_db"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 def get_db():
     try:
@@ -18,5 +20,6 @@ def get_db():
         yield db
     finally:
         db.close()
+
 
 Base = declarative_base()
--- core/api/feature.py
+++ core/api/feature.py
@@ -7,13 +7,13 @@ models.Base.metadata.create_all(bind=engine)
 
 products_test = [
     {
-        'id':1,
+        'id': 1,
         'title': 'Beajolais nouveau',
         'description': 'Vin blanc à boire en apéritif',
         'is_active': True
     },
-        {
-        'id':2,
+    {
+        'id': 2,
         'title': 'Marin d\'eau douce',
         'description': 'Vin rouge à boire en apéritif',
         'is_active': False
@@ -21,29 +21,28 @@ products_test = [
 ]
 categories_test = [
     {
-        'id':1,
+        'id': 1,
         'title': 'Vin blanc',
     },
-        {
-        'id':2,
+    {
+        'id': 2,
         'title': 'Vin rouge',
     }
 ]
 for item in products_test:
     db_products = models.Product(
-        id = item['id'],
-        title = item['title'],
-        description = item['description'],
-        is_active = item['is_active']
+        id=item['id'],
+        title=item['title'],
+        description=item['description'],
+        is_active=item['is_active']
     )
     db.add(db_products)
-    
 
 
 for item in categories_test:
     db_categories = models.Category(
-        id = item['id'],
-        title = item['title'],
+        id=item['id'],
+        title=item['title'],
     )
     db.add(db_categories)
 
